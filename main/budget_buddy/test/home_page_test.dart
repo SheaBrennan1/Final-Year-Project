4c00138da3563e1122c394466a4f4bfdacc39829
Shea Brennan <125160887+SheaBrennan1@users.noreply.github.com>
2024-04-13 23:52:08 +0100

    Update home_page_test.dart

--- main/budget_buddy/test/home_page_test.dart
+++ main/budget_buddy/test/home_page_test.dart
@@ -1,17 +1,32 @@
 import 'package:budget_buddy/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Home Widget Tests', () {
-    testWidgets('Should display the correct initial UI elements',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(MaterialApp(home: Home()));
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyCHnqK-G12Gv7EkOQnbq3TtMPWtsC04eSA",
+        appId: "1:162550313227:android:d91dd69c9c8e3505aef20e",
+        messagingSenderId: "162550313227",
+        projectId: "budget-buddy-a49f1",
+        databaseURL:
+            "https://budget-buddy-a49f1-default-rtdb.europe-west1.firebasedatabase.app/",
+        storageBucket: "budget-buddy-a49f1.appspot.com",
+      ),
+    );
+  });
 
-      // Verify that certain widgets are found in the tree.
-      expect(find.text('Total Balance'), findsOneWidget);
-      expect(find.byType(ToggleButtons), findsOneWidget);
-      // Add more checks as needed
-    });
+  testWidgets('Should display the correct initial UI elements',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: Home()));
+
+    // Ensure all async operations are completed
+    await tester.pumpAndSettle();
+
+    // Check for the presence of certain widgets.
+    expect(find.text('Total Balance'), findsOneWidget);
   });
 }
